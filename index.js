d27aba387375692fdc9f5cee15c5e1c2baff964e
RoyZiO <nziokinroy@gmail.com>
2022-11-26 18:54:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+//No. 1
+let num1 = 2
+let num2 = 31
+
+const multiply = (num1 * num2);
+
+//No. 2
+let random = 0
+++random;
+
+//No. 3
+let num3 = 10;
+let num4 = 6;
+let sum =(num3 + num4); 
+
+ let mod = sum %= 6;
+
+ mod;
+
+//No. 4
+let max = Math.max(10,12,14,16,18,20);
+max;
